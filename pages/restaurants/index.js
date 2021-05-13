4149c9883a5649ca33c5f98b707a12aa561af56c
Andréa Alencar <andreaflether@gmail.com>
2021-05-13 19:31:28 -0300

    Listing categories

--- pages/restaurants/index.js
+++ pages/restaurants/index.js
@@ -1,8 +1,10 @@
 import RestaurantsList from '../../components/RestaurantsList'
+import Categories from '../../components/Categories'
 
 export default function Restaurants() {
   return(
     <>
+      <Categories />
       <RestaurantsList />
     </>
   )
