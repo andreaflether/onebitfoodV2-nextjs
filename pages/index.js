2db035a278b4444a2a0bbb6030f0000b964c958a
Andréa Alencar <andreaflether@gmail.com>
2021-05-12 22:28:07 -0300

    Adding basic CSS stuff to the application

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,9 @@
-import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+import Container from 'react-bootstrap/Container'
 
 export default function Home() {
   return (
-    <>
-    </>
+    <Container>
+      Home page :)
+    </Container>
   )
 }
