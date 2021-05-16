813b73a1f58212616580f49dd3cb8227a2484bc1
Andréa Alencar <andreaflether@gmail.com>
2021-05-16 01:14:15 -0300

    Add SSG

--- components/RestaurantDetails/index.js
+++ components/RestaurantDetails/index.js
@@ -1,32 +1,25 @@
 import Details from './Details';
 import CategoryProducts from './CategoryProducts';
-import getRestaurant from '../../services/getRestaurant';
-import { useRouter } from 'next/router';
 import { Spinner, Alert, Col } from 'react-bootstrap';
 import { FaExclamationTriangle } from 'react-icons/fa'
 
-export default function RestaurantDetails() {
- const router = useRouter();
- const { id } = router.query;
- 
- const { restaurant, isLoading, isError } = getRestaurant(id);
- 
- if(isError) {
-   return(
+export default function RestaurantDetails(props) { 
+  /*
+  if(props.isError) {
+    return(
     <Col>
       <Alert variant="custom-red">
         <FaExclamationTriangle className="me-2 mb-1"/> Erro ao carregar este recurso.
       </Alert>
     </Col>
-   )
- } else if(isLoading) {
-   return <Col><Spinner animation="border" /></Col>
- }
+    )
+  }
+  */
   return(
     <>
-      <Details {...restaurant} />
-      {restaurant.product_categories.map((product_category, i) =>
-        <CategoryProducts restaurant={restaurant} {...product_category} key={i} />
+      <Details {...props.restaurant} />
+      {props.restaurant.product_categories.map((product_category, i) =>
+        <CategoryProducts restaurant={props.restaurant} {...product_category} key={i} />
       )}
     </>
   )
